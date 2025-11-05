4bdad5fd2702f20d9ba8d911581aea66a801a503
Michael Taylor <michael@jigsaw.digital>
2025-11-05 12:24:13 +0400

    [wip]

--- wp-content/themes/sx/header.php
+++ wp-content/themes/sx/header.php
@@ -9,7 +9,11 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-    
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.svg">
+    <link rel="alternate icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.svg">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
